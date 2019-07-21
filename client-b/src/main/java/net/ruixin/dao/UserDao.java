fa6aaad3650aa8d9f423f845df8f2411b01e423d
yucheng <yujun04072010@163.com>
2019-07-22 01:26:19 +0800

    Elasticsearch

--- client-b/src/main/java/net/ruixin/dao/UserDao.java
+++ client-b/src/main/java/net/ruixin/dao/UserDao.java
@@ -1,6 +1,13 @@
 package net.ruixin.dao;
 
 import net.ruixin.domain.entity.User;
+
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,12 +15,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 import java.util.Map;
 
+
 /**
  * Created by yucheng on 2019/7/18.
  */
 @Repository
-public class UserDao {
+public class UserDao{
+    @Autowired
+    EsUserDao esUserDao;
+
     public User getUser(String account, String password) {
+        Pageable pageable = new PageRequest(0,10);
+        //按标题进行搜索
+        MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("account", "AD");
+//        QueryBuilder queryBuilder = QueryBuilders.matchQuery("account", account);
+        esUserDao.search(matchQueryBuilder);
         return null;
     }
 
@@ -22,6 +38,7 @@ public class UserDao {
     }
 
     public List<User> getUserList(Map map) {
+
         return null;
     }
 }
