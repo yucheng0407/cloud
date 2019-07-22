b661604e3d9fe0083fe5961bcbf9f5dad030bb0f
yucheng <yujun04072010@163.com>
2019-07-22 20:12:36 +0800

    添加拦截器

---
+++ client-b/src/main/java/net/ruixin/intercepors/SessionConfiguration.java
@@ -0,0 +1,17 @@
+package net.ruixin.intercepors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by 20160216 on 2018/2/8.
+ */
+@Configuration
+public class SessionConfiguration extends WebMvcConfigurerAdapter
+{
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
+    }
+}
--- server-center/src/main/java/net/ruixin/MyFilter.java
+++ server-center/src/main/java/net/ruixin/MyFilter.java
@@ -1,15 +1,21 @@
 package net.ruixin;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
 @Component
 public class MyFilter extends ZuulFilter {
-
+    private static final long EXPIRE_TIME = 1 * 60 * 1000;
     private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
     @Override
     public String filterType() {
         return "pre";
@@ -31,16 +37,21 @@ public class MyFilter extends ZuulFilter {
         HttpServletRequest request = ctx.getRequest();
 //        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
         Object accessToken = request.getParameter("token");
-        if(accessToken == null) {
+        if (accessToken == null) {
             log.warn("token is empty");
             ctx.setSendZuulResponse(false);
             ctx.setResponseStatusCode(401);
             try {
                 ctx.getResponse().getWriter().write("token is empty");
-            }catch (Exception e){}
+            } catch (Exception e) {
+            }
 
             return null;
         }
+        // 设置过期时间
+        Date date = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+        String token = JWT.create().withAudience("1231").withExpiresAt(date).sign(Algorithm.HMAC256("yucheng697094"));
+        ctx.addZuulRequestHeader("token", token);
         log.info("ok");
         return null;
     }
