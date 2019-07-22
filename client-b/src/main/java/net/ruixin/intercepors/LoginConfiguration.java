094b56625536bf88b353f4b92e61cf25bffa018a
yucheng <yujun04072010@163.com>
2019-07-22 20:14:24 +0800

    修改名字

--- client-b/src/main/java/net/ruixin/intercepors/LoginConfiguration.java
+++ client-b/src/main/java/net/ruixin/intercepors/LoginConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by 20160216 on 2018/2/8.
  */
 @Configuration
-public class SessionConfiguration extends WebMvcConfigurerAdapter
+public class LoginConfiguration extends WebMvcConfigurerAdapter
 {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
